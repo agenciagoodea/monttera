26b0c58fe6c1d7ff8e669acb52cf3c404a975efc
agenciagoodea <agenciagoodea@gmail.com>
2026-07-16 00:53:57 -0400

    feat: use import instead of require in app.js for ES module compatibility

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 // Entrypoint Wrapper para DirectAdmin e Phusion Passenger
 // O Passenger por padrão busca arquivos como app.js ou server.js na raiz.
 // Este arquivo simplesmente repassa a execução para o build correto dentro de dist/.
-require('./dist/server.cjs');
+import './dist/server.cjs';
